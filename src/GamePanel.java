d61a2874785d795d6e58a67c8465da0d651c1d81
Nabin Rai <you@example.com>
2023-01-29 23:22:56 +0545

    Initial
    First Commit

---
+++ src/GamePanel.java
@@ -0,0 +1,44 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+	GamePanel(){
+		
+	}
+	public void startGame() {
+		
+	}
+	public void paintComponents(Graphics g) {
+		
+	}
+	public void draw(Graphics g) {
+		
+	}
+	public void move() {
+		
+	}
+	public void checkApple() {
+		
+	}
+	public void checkCollisions() {
+		
+	}
+	public void gameOver() {
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	public class MyKeyAdapter extends KeyAdapter{
+		
+	}
+
+}
+ 
---
+++ src/SnakeMain.java
@@ -0,0 +1,10 @@
+
+public class SnakeMain {
+	public static void main(String[] args) {
+//		GameFrame frame = new GameFrame();
+//		but if u dont plan to use frame i.e just need and instance we can do
+		new GameFrame();
+		
+		
+	}
+}
