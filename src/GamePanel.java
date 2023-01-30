8e17bdc6b66960e520692a7bb30b4cfbd42c0bd8
Nabin Rai <rainabinkhyal@gmail.com>
2023-01-30 15:56:11 +0545

    Constructor Completed

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -24,7 +26,12 @@ public class GamePanel extends JPanel implements ActionListener{
 	Random random;
 	
 	GamePanel(){
-		
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
 	}
 	public void startGame() {
 		newApple(); //as game starts we create a new apple
@@ -33,12 +40,13 @@ public class GamePanel extends JPanel implements ActionListener{
 		timer.start();
 	}
 	public void paintComponents(Graphics g) {
-		
+		super.paintComponents(g);
+		draw(g);
 	}
 	public void draw(Graphics g) {
 		
 	}
-	public void newApple() {
+	public void newApple() { 
 		
 	}
 	public void move() {
