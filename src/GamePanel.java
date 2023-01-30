dafadb3767f08289cfa7e228381a20e8d07d028c
Nabin Rai <rainabinkhyal@gmail.com>
2023-01-31 00:09:07 +0545

    Collision Checking updated

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -108,7 +108,35 @@ public class GamePanel extends JPanel implements ActionListener{
 		
 	}
 	public void checkCollisions() {
+		//checks if head collides with body
+		for(int i = bodyParts; i > 0; i--) {
+			if((x[0] == x[i]) && (y[0] == y[i])) {
+				running = false;
+			}
+		}
 		
+		//check if head touches left border
+		if(x[0] < 0) {
+			running = false;
+		}
+		
+		//check if head touches right border
+		if(x[0] > SCREEN_WIDTH) {
+			running = false;
+		}
+		
+		//check if head touches top border
+		if(y[0] < 0) {
+			running = false;
+		}
+		
+		//check if head touches buttom border
+		if(y[0] > SCREEN_HEIGHT) {
+			running = false;
+		}
+		if(!running) {
+			timer.stop();
+		}
 	}
 	public void gameOver() {
 		
